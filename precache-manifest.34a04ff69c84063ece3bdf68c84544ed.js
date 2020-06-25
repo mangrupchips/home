977bec371b25a7d7aa9711b8ea62e5764b5a20f0
Tanapon <tanapon@renthub.in.th>
2020-06-26 00:42:13 +0700

    Updates

---
+++ precache-manifest.34a04ff69c84063ece3bdf68c84544ed.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "493bc7e82845f0b129d393fd0153a750",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "743991be0aafa4e0092f",
+    "url": "/home/static/css/main.ede9ec2f.chunk.css"
+  },
+  {
+    "revision": "e035ae0530fa729543ed",
+    "url": "/home/static/js/2.8ef3efb7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/home/static/js/2.8ef3efb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "743991be0aafa4e0092f",
+    "url": "/home/static/js/main.3ba3e9ff.chunk.js"
+  },
+  {
+    "revision": "fc8839b25a56ae2bd188",
+    "url": "/home/static/js/runtime-main.0781c2b0.js"
+  },
+  {
+    "revision": "972549153c3b9b32869de9bb5837c93c",
+    "url": "/home/static/media/igicon.97254915.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/home/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fc6797d7996072d22a03b73cc37cf10e",
+    "url": "/home/static/media/maaja_ver_1.00.fc6797d7.ttf"
+  },
+  {
+    "revision": "6d4be742d6c104c03d80a3687bc9c2ca",
+    "url": "/home/static/media/mangrupchips-durian.6d4be742.png"
+  }
+]);
