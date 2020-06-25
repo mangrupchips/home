2528f4d248828eb93a96595f3faf4298ca4c801b
Tanapon <tanapon@renthub.in.th>
2020-06-26 01:33:46 +0700

    done: design 2

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React from "react";
 import logo from "./logo.svg";
 import igIcon from "./igicon.png";
 import durian from "./mangrupchips-durian.png";
+import banana from "./banana.png";
+import jackfruit from "./jackfruit.png";
+import click from "./click.png";
 import styled, { css } from "styled-components";
 import "./App.css";
 
@@ -9,15 +12,32 @@ function App() {
   return (
     <div className='App'>
       <header className='App-header'>
-        <a href='https://www.instagram.com/mangrupchips'>
-          <img src={durian} className='App-logo' alt='logo' />
-        </a>
+        <LogoContainer>
+          <a href='https://www.instagram.com/mangrupchips'>
+            <div>
+              <img src={jackfruit} className='App-logo' alt='logo' />
+            </div>
+          </a>
+          <a href='https://www.instagram.com/mangrupchips'>
+            <div>
+              <img src={durian} className='App-logo' alt='logo' />
+            </div>
+          </a>
+          <a href='https://www.instagram.com/mangrupchips'>
+            <div>
+              <img src={banana} className='App-logo' alt='logo' />
+            </div>
+          </a>
+        </LogoContainer>
+
         <Title>
           <a href='https://www.instagram.com/mangrupchips'>Mangrupchips</a>
           <br />
           <span>Coming Soon!! </span>
         </Title>
-        <Description>แล้วพบกับเว็บไซต์เต็มรูปแบบเร็ว ๆ นี้จ้า</Description>
+        <ClickIcon>
+          <img src={click} alt='logo' />
+        </ClickIcon>
         <IgContainer>
           <a href='https://www.instagram.com/mangrupchips'>
             <img src={igIcon} className='ig-logo' alt='ig-logo' />
@@ -38,9 +58,22 @@ const IgContainer = styled.div`
   justify-content: center;
 `;
 
+const LogoContainer = styled.div`
+  display: flex;
+  width: 80%;
+  justify-content: center;
+  div {
+    width: 80%;
+    img {
+      width: 100px;
+      height: 100px;
+    }
+  }
+`;
+
 const Title = styled.h1`
   a {
-    font-size: 4.5rem;
+    font-size: 4.2rem;
     color: #f0a500;
     text-decoration: none;
     :hover {
@@ -52,15 +85,21 @@ const Title = styled.h1`
   }
 `;
 
-const Description = styled.h2`
-  color: #cf7500;
-`;
-
 const IgLink = styled.a`
   color: #f4f4f4;
+  font-size: 30px;
   margin: auto 0;
   text-decoration: none;
   :hover {
     text-decoration: underline;
+  }
+`;
+
+const ClickIcon = styled.div`
+  width: 100px;
+  height: 20px;
+  margin-bottom: -20px;
+  img {
+    width: 100%;
   }
 `;
